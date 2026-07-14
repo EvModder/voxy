b164a6d98c378ebb6bbb3e538770d76d527c001f
mcrcortex <18544518+mcrcortex@users.noreply.github.com>
2026-07-15 00:52:38 +1000

    version update, use world ref track in world ingest, finalize some classes

--- src/main/java/me/cortex/voxy/common/world/ActiveSectionTracker.java
+++ src/main/java/me/cortex/voxy/common/world/ActiveSectionTracker.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.StampedLock;
 
-public class ActiveSectionTracker {
+public final class ActiveSectionTracker {
 
     //Deserialize into the supplied section, returns true on success, false on failure
     public interface SectionLoader {int load(WorldSection section);}
